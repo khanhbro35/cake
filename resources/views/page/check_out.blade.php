c0b48e08cd0562bf0115d53ce829d5e0e7636081
khanhd <khanhbro35@gmail.com>
2021-12-29 15:03:39 +0700

    hien loi form

--- resources/views/page/check_out.blade.php
+++ resources/views/page/check_out.blade.php
@@ -36,6 +36,22 @@
                             here</a> to enter your code</h6>
                             <h6 class="checkout__title">Billing Details</h6>
                             <div class="row">
+                                    <div class="col-lg-12">
+                                            @if($errors->any())
+                                                <div class="alert alert-danger alert-dismissible fade show">
+                                                    <ul>
+                                                        @foreach ($errors->all() as $error)
+                                                            <li>{{ $error }}</li>
+                                                        @endforeach
+                                                    </ul>
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                            @endif
+                                    </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-lg-6">
                                     <div class="checkout__input">
                                         <p>Fist Name<span>*</span></p>
